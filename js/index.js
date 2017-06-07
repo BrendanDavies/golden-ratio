07ac317cc714051e43f1fcf36bb21e7096a4646d
Brendan Davies <brendankyledavies@gmail.com>
2017-06-06 22:24:24 -0400

    Handle directional buttons

--- js/index.js
+++ js/index.js
@@ -61,6 +61,10 @@
       const scale = aspect ** index;
       const dimmedColor = Math.floor(255 - (index * (255 / sectionCount)));
 
+      if (index === 0) {
+        section.classList.add('active');
+      }
+
       section.style.width = dimensions.sectionSize;
       section.style.height = dimensions.sectionSize;
       section.style.transformOrigin = spiralOrigin;
@@ -69,15 +73,84 @@
     });
   }
 
+  /**
+   * Clear active flag from any sections
+   */
+  function clearActiveSections() {
+    window.document.querySelectorAll('.js-section.active').forEach((activeSection) => {
+      activeSection.classList.remove('active');
+    });
+  }
+
+  /**
+   * Returns the index of the first active solution
+   */
+  function getActiveSectionIndex() {
+    let activeIndex;
+    sections.forEach((section, index) => {
+      if (section.classList.contains('active')) {
+        activeIndex = index;
+      }
+    });
+    return activeIndex;
+  }
+
+  /**
+   * Handles selecting an section
+   * @param {Number} index - index of item to select
+   */
+  function scrollToItem(index) {
+    return () => {
+      const rotation = -90 * index;
+      const scale = aspect ** (rotation / 90);
+
+      if (index >= 0 && index < sections.length) {
+        clearActiveSections();
+        sections[index].classList.add('active');
+        spiral.style.transform = `rotate(${rotation}deg) scale(${scale})`;
+      }
+    };
+  }
+
   buildSpiral(getSpiralDimensions());
+
+// Resize Handler
+  window.addEventListener('resize', () => buildSpiral(getSpiralDimensions()));
 
 // Click Handler
 // TODO: USE DELEGATE LISTENER
   sections.forEach((section, index) => {
-    section.addEventListener('click', () => {
-      const rotation = -90 * index;
-      const scale = aspect ** (rotation / 90);
-      spiral.style.transform = `rotate(${rotation}deg) scale(${scale})`;
-    });
+    section.addEventListener('click', scrollToItem(index));
+  });
+
+// Directional Keys
+  const KEY_CODES = {
+    SPACE: 32,
+    LEFT: 37,
+    UP: 38,
+    RIGHT: 39,
+    DOWN: 40,
+  };
+
+  window.addEventListener('keydown', (e) => {
+    const FORWARD_KEYS = [
+      KEY_CODES.DOWN,
+      KEY_CODES.RIGHT,
+      KEY_CODES.SPACE,
+    ];
+    const BACK_KEYS = [
+      KEY_CODES.LEFT,
+      KEY_CODES.UP,
+    ];
+    const currentActiveSection = getActiveSectionIndex();
+    let indexToSelect;
+
+    if (FORWARD_KEYS.indexOf(e.keyCode) !== -1) {
+      indexToSelect = currentActiveSection + 1;
+    } else if (BACK_KEYS.indexOf(e.keyCode) !== -1) {
+      indexToSelect = currentActiveSection - 1;
+    }
+
+    scrollToItem(indexToSelect)();
   });
 }());
